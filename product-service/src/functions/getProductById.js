0fdd6492f9b659b7ff69dd087a5d9192a827f6b0
davalenzblanc <davalenzblanc@gmail.com>
2024-05-09 21:50:27 -0500

    get by id endpoint

--- product-service/src/functions/getProductById.js
+++ product-service/src/functions/getProductById.js
@@ -1,18 +1,38 @@
-import { formatJSONResponse } from '../utils';
-import { products } from '../mocks/products';
+import AWS from "aws-sdk";
+import { formatJSONResponse } from "../utils";
+
+const dynamodb = new AWS.DynamoDB();
+const tableName = "shop-products";
 
 export const getProductByIdHandler = async (event) => {
   try {
     const { productId } = event.pathParameters;
-    const product = products.find((product) => product.id === productId);
-    if (!product) {
+
+    const params = {
+      TableName: tableName,
+      Key: {
+        id: { S: productId },
+      },
+    };
+
+    const productData = await dynamodb.getItem(params).promise();
+
+    if (!productData.Item) {
       return formatJSONResponse(
         {
-          message: 'Product not found',
+          message: "Product not found",
         },
         404
       );
     }
+
+    const product = {
+      id: productData.Item.id.S,
+      title: productData.Item.title.S,
+      description: productData.Item.description.S,
+      price: productData.Item.price.N,
+    };
+
     return formatJSONResponse(product);
   } catch (error) {
     return formatJSONResponse(
